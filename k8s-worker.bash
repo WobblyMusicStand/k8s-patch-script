f0c2114617d2e3a4b1541b482b11ab91cb6018c5
WobblyMusicStand <WobblyMusicStand@gmail.com>
2022-01-19 17:21:35 -0800

    Updated to use $(hostname) for identification

--- k8s-worker.bash
+++ k8s-worker.bash
@@ -4,9 +4,9 @@ echo 'Passw0rd!' | sudo -S kubeadm reset --force --ignore-preflight-errors strin
 
 if sudo @lab.Variable(k8sToken) 2>&1 | grep -q -F 'This node has joined the cluster'; 
 then
-    printf "[Worker 2 Join] Successfully joined cluster \n"
+    printf "[%s Join] Successfully joined cluster \n" "$(hostname)"
     echo true
 else
-    printf "[Worker 2 Join] Failed to join cluster with token: @lab.Variable(k8sToken) \n" 1>&2
+    printf "[%s Join] Failed to join cluster with token: @lab.Variable(k8sToken) \n" "$(hostname)" 1>&2
     echo false
 fi
